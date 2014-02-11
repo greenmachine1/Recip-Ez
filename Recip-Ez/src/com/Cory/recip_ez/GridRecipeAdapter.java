66d38917ff2e7546ea2d5f5c40d9c3d8aea7254c
greenmachine1 <scary_penguins05@hotmail.com>
2014-02-10 19:40:52 -0800

    modified my grid view adapter

--- Recip-Ez/src/com/Cory/recip_ez/GridRecipeAdapter.java
+++ Recip-Ez/src/com/Cory/recip_ez/GridRecipeAdapter.java
@@ -4,38 +4,58 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
-public class GridRecipeAdapter extends BaseAdapter {
+// -- Grid adapter section -- //
+public class GridRecipeAdapter extends BaseAdapter{
 	
 	private Context context;
+	public Integer[] listOfImages = null;
 	
-	// constructor
-	public GridRecipeAdapter(Context _context){
-		context = _context;
+	public GridRecipeAdapter(Context c, Integer[] imageIDs){
+		context = c;
+		listOfImages = imageIDs;
 	}
 
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return listOfImages.length;
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		return null;
+		return position;
 	}
 
 	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
 
+	
+	// setting the image to the individual squares
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		ImageView imageView;
+		if(convertView == null){
+			
+			imageView = new ImageView(context);
+			imageView.setLayoutParams(new GridView.LayoutParams(200,200));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
+			imageView.setPadding(10, 10, 10, 10);
+		
+		}else{
+			imageView = (ImageView) convertView;
+		}
+		imageView.setImageResource(listOfImages[position]);
+		return imageView;
+
+	}
+	
 }
--- Recip-Ez/src/com/Cory/recip_ez/GridViewFragment.java
+++ Recip-Ez/src/com/Cory/recip_ez/GridViewFragment.java
@@ -1,15 +1,33 @@
 package com.Cory.recip_ez;
 
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class GridViewFragment extends Fragment{
 	
+	
+
+	
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		
+		// place holders for images
+		Integer[] imageIDs = {
+			R.drawable.main_logo,
+			R.drawable.ic_share,
+			R.drawable.ic_launcher,
+			R.drawable.ic_ingredients_search
+		};
+		
 		
 		View view;
 		
@@ -17,13 +35,17 @@ public class GridViewFragment extends Fragment{
 		
 		GridView gridView = (GridView)view.findViewById(R.id.gridView1);
 		
+		// setting the gridview adapter
+		gridView.setAdapter(new GridRecipeAdapter(getActivity(), imageIDs));
 		
-		
-
-
 		return view;
 		
 	}
+	
+	
+	
+	
+
 
 
 	
