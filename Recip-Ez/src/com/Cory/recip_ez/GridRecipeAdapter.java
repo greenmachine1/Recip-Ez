ce25cd7cfbf029fd979bb339e71e3d47f6fc8136
greenmachine1 <scary_penguins05@hotmail.com>
2014-02-10 21:13:29 -0800

    had to adjust my layout of my grid items.  But it works pretty well now

--- Recip-Ez/src/com/Cory/recip_ez/GridRecipeAdapter.java
+++ Recip-Ez/src/com/Cory/recip_ez/GridRecipeAdapter.java
@@ -2,9 +2,14 @@ package com.Cory.recip_ez;
 
 import java.util.List;
 
+import android.app.Activity;
 import android.content.Context;
-import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 
@@ -23,6 +28,49 @@ public class GridRecipeAdapter extends ArrayAdapter<GridViewAdapterDefinition>{
 		this.layoutResourceId = layoutResourceId;
 		this.context = context;
 		this.data = data;
+		
+	}
+	
+	
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent){
+		
+		View row = convertView;
+		GridRecipeAdapterHolder holder = null;
+		
+		
+		if(row == null){
+			LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+			row = inflater.inflate(layoutResourceId, parent, false);
+			
+			holder = new GridRecipeAdapterHolder();
+			
+			holder.imgIcon = (ImageView)row.findViewById(R.id.recipe_image);
+			holder.txtTitle = (TextView)row.findViewById(R.id.title_text);
+
+			
+			row.setTag(holder);
+		}else{
+			
+			holder = (GridRecipeAdapterHolder)row.getTag();
+			
+		}
+		
+		GridViewAdapterDefinition gridRecipeAdapter = getItem(position);
+		
+		holder.txtTitle.setText(gridRecipeAdapter.recipeTitle);
+		holder.imgIcon.setImageResource(gridRecipeAdapter.mainImage);
+
+		return row;
+		
+	}
+	
+	static class GridRecipeAdapterHolder
+	{
+		ImageView imgIcon;
+		TextView txtTitle;
+
 		
 	}
 
