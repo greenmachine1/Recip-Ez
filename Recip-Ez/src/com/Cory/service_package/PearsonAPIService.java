09a8d7e5c4211710b1823fbe20e0c46c419846de
greenmachine1 <scary_penguins05@hotmail.com>
2014-02-14 00:22:24 -0800

    made a package for my json service portion

---
+++ Recip-Ez/src/com/Cory/service_package/PearsonAPIService.java
@@ -0,0 +1,19 @@
+package com.Cory.service_package;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class PearsonAPIService extends IntentService {
+
+	public PearsonAPIService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
