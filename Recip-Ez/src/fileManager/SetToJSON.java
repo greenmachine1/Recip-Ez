7a5dcde39b75817af95838b7cecabd97c5c13ce8
greenmachine1 <scary_penguins05@hotmail.com>
2014-02-20 22:09:40 -0800

    setting up my to json class

---
+++ Recip-Ez/src/fileManager/SetToJSON.java
@@ -0,0 +1,21 @@
+package fileManager;
+
+import android.content.Context;
+
+
+// -- class thats set up to gather information and convert
+// -- it to json then save it
+public class SetToJSON {
+	
+	Context _context;
+	
+	public final String NAME_OF_FILE = "favoritesJSON.txt";
+
+	public SetToJSON(Context context) {
+		_context = context;
+	}
+	
+	
+	
+	
+}
