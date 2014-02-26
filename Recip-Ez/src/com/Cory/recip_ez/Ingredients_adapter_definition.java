ae09f5cca546eca88d58d800ac9e02314f6ba27c
greenmachine1 <scary_penguins05@hotmail.com>
2014-02-25 17:48:11 -0800

    finished my ingredients definition for the adapter

--- Recip-Ez/src/com/Cory/recip_ez/Ingredients_adapter_definition.java
+++ Recip-Ez/src/com/Cory/recip_ez/Ingredients_adapter_definition.java
@@ -2,4 +2,19 @@ package com.Cory.recip_ez;
 
 public class Ingredients_adapter_definition {
 
+	public String mainTextView;
+	public String checkIcon;
+	
+	
+	public Ingredients_adapter_definition(){
+		super();
+	}
+	
+	public Ingredients_adapter_definition(String textViewText, String iconString){
+		super();
+		
+		this.mainTextView = textViewText;
+		this.checkIcon = iconString;
+	}
+	
 }
